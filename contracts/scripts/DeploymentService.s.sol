15075693f7e05367edad4eadc6d3aba329bcd6ce
gitmp01 <mauro.piazza.01@protonmail.com>
2025-01-17 21:40:30 +0100

    chore(DeploymentService.s.sol): add deterministic deployment

--- contracts/scripts/DeploymentService.s.sol
+++ contracts/scripts/DeploymentService.s.sol
@@ -1,38 +1,66 @@
 pragma solidity ^0.8.28;
 
 import {SafeModule} from '../src/SafeModule.sol';
-import {CreateXScript} from 'createx-forge/script/CreateXScript.sol';
+import {TestToken} from '../src/test/TestToken.sol';
+import {ICreateX, CreateXScript} from 'createx-forge/script/CreateXScript.sol';
+import {ERC1967Proxy} from '@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol';
 
 import {console} from 'forge-std/console.sol';
+
 /**
  * Deploys a new UD safe
  */
 contract DeploymentService is CreateXScript {
 	address legacyAddress = vm.addr(1);
+	address owner = 0xf9A9e6288c7A23B2b07f06f668084A1101835fA6;
+	address dlnSource = 0xeF4fB24aD0916217251F553c0596F8Edc630EB66;
 
 	function setUp() public withCreateX {}
 
 	function run() public {
-		// vm.startBroadcast();
 		address deployer = msg.sender;
-		bytes memory bytecode = type(SafeModule).creationCode;
-		bytes32 byteCodeDigest = keccak256(bytecode);
 
-		bytes32 salt = bytes32(abi.encodePacked(legacyAddress));
-		address safeModuleAddress = computeCreate2Address(salt, byteCodeDigest);
+		console.log('CreateX', address(CreateX));
+		console.log('Deployer @', deployer);
 
-		console.log('Deployer:', deployer);
+		vm.startBroadcast();
+
+		TestToken token = new TestToken();
+		SafeModule logicAddress = new SafeModule();
+		console.log('Logic address @', address(logicAddress));
+
+		bytes memory initializeData = abi.encodeWithSignature(
+			'initialize(address,address,address)',
+			owner,
+			address(token),
+			dlnSource
+		);
+
+		bytes memory initCode = abi.encodePacked(
+			type(ERC1967Proxy).creationCode,
+			abi.encode(address(logicAddress), initializeData)
+		);
+
+		bytes32 salt = bytes32(bytes.concat(abi.encodePacked(legacyAddress), bytes1(0x00)));
+		address safeModuleAddress = CreateX.computeCreate2Address(
+			keccak256(abi.encode(salt)), // as per CreateX._guardedSalt()
+			keccak256(initCode)
+		);
+
 		console.log('Expected SafeModule address', safeModuleAddress);
 
-		// SafeModule module = new SafeModule();
-
-		// // Calculate the predetermined address of the Counter contract deployment
-		// address computedAddress = computeCreate2Address(salt, deployer);
-
-		// console.log('Proxy computed contract address:', computedAddress);
-
-		// bytes memory initializeData = abi.encodeWithSignature(
-		// 	'initailize(address,address,address)'
+		// ICreateX.Values memory values = ICreateX.Values(0.01 ether, 0.01 ether);
+		// address module = CreateX.deployCreate2AndInit{value: 0.03 ether}(
+		// 	salt,
+		// 	bytecode,
+		// 	constructorData,
+		// 	values,
+		// 	msg.sender
 		// );
+
+		address module = CreateX.deployCreate2(salt, initCode);
+
+		assert(module == safeModuleAddress);
+		assert(SafeModule(safeModuleAddress).owner() == owner);
 	}
 }
