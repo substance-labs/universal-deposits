ca9abc6024dbadb68f147b6a7eb50b0e486a4c6d
gitmp01 <mauro.piazza.01@protonmail.com>
2025-01-30 14:38:07 +0100

    chore(sdk): add bundle build

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,7 +2,7 @@ import { Address } from 'viem'
 
 import { UniversalDeposits } from './lib/ud'
 
-const getUDAddress = (destinationAddress: Address) => {
+export default function getUDAddress(destinationAddress: Address) {
   // const destinationAddress = '0xf9A9e6288c7A23B2b07f06f668084A1101835fA6'
   const destinationToken = '0xcB444e90D8198415266c6a2724b7900fb12FC56E'
   const destinationChain = BigInt('100000002')
@@ -16,4 +16,4 @@ const getUDAddress = (destinationAddress: Address) => {
   return ud.getUDSafeAddress()
 }
 
-export default getUDAddress
+window.getUDAddress = getUDAddress
